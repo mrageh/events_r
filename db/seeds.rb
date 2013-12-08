fc6fade7ab2cd119290f6c03658fd4c7b80730b8
Adam89 <Adamm1@aston.ac.uk>
2013-12-08 14:50:07 -0700

    created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.create!([
+  {
+    name: "BugSmash",
+    location: "Denver",
+    price: 0,
+    description: 'A fun evening of bug smashing!',
+    starts_at: 10.days.from_now
+    },
+    {
+    name: "Hackathon",
+    location: "Austin",
+    price: 15.00,
+    description: 'Hunker down at the Hackathon!',
+    starts_at: 15.days.from_now
+    },
+    {
+    name: "Kata Camp",
+    location: "Dallas",
+    price: 75.00,
+    description: 'Practice your craft kata style!',
+    starts_at: 30.days.from_now
+    }
+  ])
