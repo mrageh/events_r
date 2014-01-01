9ff4771b2d38966b932b5c1931ebbd7f67c219f9
Adam89 <Adamm1@aston.ac.uk>
2014-01-01 13:59:39 -0700

    users can now register for events

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,24 @@ class RegistrationsController < ApplicationController
     @registrations = @event.registrations.all
   end
 
+  def new
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.create(registration_params)
+    if @registration.save
+      redirect_to event_registrations_path(@event), notice: "Thanks for registering!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def registration_params
+    params.require(:registration).permit(:full_name, :email, :how_heard)
+  end
 end
